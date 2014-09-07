030e95b22cb5d2d0682b4b90e2dc26269490b8aa
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 12:34:40 +0200

    Added the HelloWorld middleware

---
+++ HelloWorld/HelloWorld/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var middleware = new Func<AppFunc, AppFunc>(CreateMiddleware);
+
+            app.Use(middleware);
+        }
+
+        private static AppFunc CreateMiddleware(AppFunc next)
+        {
+            AppFunc middleware = environment =>
+            {
+                var response = (Stream)environment["owin.ResponseBody"];
+                var bytes = Encoding.UTF8.GetBytes("Hello, world!");
+
+                response.WriteAsync(bytes, 0, bytes.Length);
+
+                return next(environment);
+            };
+
+            return middleware;
+        }
+    }
+}
