81c0de65e77184827b96304ad47c53f59713d3db
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 12:41:37 +0200

    Added strongly-typed OWIN context

--- HelloWorld/HelloWorld/Startup.cs
+++ HelloWorld/HelloWorld/Startup.cs
@@ -1,40 +1,22 @@
 ﻿using Owin;
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    using AppFunc = Func<IDictionary<string, object>, Task>;
-
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            var middleware = new Func<AppFunc, AppFunc>(CreateMiddleware);
-
-            app.Use(middleware);
-        }
-
-        private static AppFunc CreateMiddleware(AppFunc next)
-        {
-            AppFunc middleware = environment =>
+            app.Run(context =>
             {
-                var response = (Stream)environment["owin.ResponseBody"];
+                var response = context.Response;
                 var bytes = Encoding.UTF8.GetBytes("<html><body><h1>Hello, world!</h1></body></html>");
-                var headers = (IDictionary<string, string[]>)environment["owin.ResponseHeaders"];
 
-                headers["Content-Length"] = new[] { bytes.Length.ToString(CultureInfo.InvariantCulture) };
-                headers["Content-Type"] = new[] { "text/html" };
-                response.WriteAsync(bytes, 0, bytes.Length);
+                response.ContentLength = bytes.Length;
+                response.ContentType = "text/html";
 
-                return next(environment);
-            };
-
-            return middleware;
+                return response.WriteAsync(bytes);
+            });
         }
     }
 }
