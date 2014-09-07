828114477c164e251147ab6ac025317c81fbd60d
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 13:43:46 +0200

    Updated app with a condition on request path value

--- Conditionals/Conditionals/Startup.cs
+++ Conditionals/Conditionals/Startup.cs
@@ -1,16 +1,34 @@
 ﻿using Owin;
+using System;
+using System.Linq;
 
 namespace Conditionals
 {
     public class Startup
     {
+        private static readonly string[] PeopleFromMic = { "Martine", "Renaud", "Thomas", "Xavier" };
+
         public void Configuration(IAppBuilder app)
         {
-            app.Map("/mic", nestedApp =>
+            app.Map("/mic", micApp =>
             {
-                nestedApp.Map("/renaud", renaudApp => renaudApp.Run(context => context.Response.WriteAsync("Hello, Renaud!")));
+                micApp.MapWhen(context =>
+                {
+                    if (context.Request.Path.HasValue)
+                    {
+                        var name = context.Request.Path.Value.Trim('/');
+                        return PeopleFromMic.Any(item => StringComparer.InvariantCultureIgnoreCase.Equals(name, item));
+                    }
 
-                nestedApp.Run(context => context.Response.WriteAsync("Hello, Microsoft Innovation Center!\r\n"));
+                    return false;
+                },
+                micPeopleApp => micPeopleApp.Run(context =>
+                {
+                    var name = context.Request.Path.Value.Trim('/');
+                    return context.Response.WriteAsync(String.Format("Hello, {0}\r\n", name));
+                }));
+
+                micApp.Run(context => context.Response.WriteAsync("Hello, Microsoft Innovation Center!\r\n"));
             });
 
             app.Run(context => context.Response.WriteAsync("Hello, World!\r\n"));
