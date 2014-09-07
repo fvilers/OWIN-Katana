92965e921da8e44a53744510df5f4dc2a96c9f60
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 13:02:54 +0200

    Updated middlewares to display execution time

--- Chaining/Chaining/Startup.cs
+++ Chaining/Chaining/Startup.cs
@@ -1,19 +1,42 @@
 ﻿using Owin;
+using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Chaining
 {
     public class Startup
     {
+        private static readonly Random Randomizer = new Random();
+
         public void Configuration(IAppBuilder app)
         {
+            app.Use(async (context, next) =>
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+
+                await next.Invoke();
+                sw.Stop();
+
+                await context.Response.WriteAsync(String.Format("The pipeline execution took {0} ms\r\n", sw.ElapsedMilliseconds));
+            });
+
             app.Use((context, next) =>
             {
-                context.Response.WriteAsync("I'm middleware #1");
+                context.Response.WriteAsync("I'm middleware #1\r\n");
+                Thread.Sleep(Randomizer.Next(500));
 
                 return next.Invoke();
             });
 
-            app.Run(context => context.Response.WriteAsync(" - I'm middleware #2"));
+            app.Use((context, next) =>
+            {
+                context.Response.WriteAsync("I'm middleware #2\r\n");
+                Thread.Sleep(Randomizer.Next(1000));
+
+                return next.Invoke();
+            });
         }
     }
 }
