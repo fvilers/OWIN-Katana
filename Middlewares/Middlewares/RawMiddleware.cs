348e175ffc82b7ff19d4e36a9f7500670fedb0f2
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 13:52:36 +0200

    Added an empy raw middleware

---
+++ Middlewares/Middlewares/RawMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Middlewares
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class RawMiddleware
+    {
+        private readonly AppFunc _next;
+
+        public RawMiddleware(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            await _next(environment);
+        }
+    }
+}
---
+++ Middlewares/Middlewares/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Middlewares
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Use<RawMiddleware>();
+        }
+    }
+}
