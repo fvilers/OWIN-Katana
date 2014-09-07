c06722bdc7ee987ad2ac078976f3248bf60ee351
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 17:22:02 +0200

    Added a console host to test our OWIN pipeline

---
+++ CompleteSample/CompleteSample.ConsoleHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using CompleteSample.Api.Controllers;
+using Microsoft.Owin.Hosting;
+using System;
+
+namespace CompleteSample.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This ensures that the Middlewares assembly gets loaded
+            var controllerType = typeof(ProductController);
+
+            const string url = "http://localhost:5000";
+
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Host is listening on {0}... Press any key to quit.", url);
+                Console.ReadLine();
+            }
+        }
+    }
+}
