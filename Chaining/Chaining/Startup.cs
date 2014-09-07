b1059ff15d1f59e58517cdd04211f412c4c9a2ca
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 12:48:14 +0200

    Added two middlewares with a short-circuit

---
+++ Chaining/Chaining/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+namespace Chaining
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context => context.Response.WriteAsync("I'm middleware #1"));
+            app.Run(context => context.Response.WriteAsync("I'm middleware #2"));
+        }
+    }
+}
