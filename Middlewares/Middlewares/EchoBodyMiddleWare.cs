84700f83ee2e4de89daf9951b23fe19b31e19a97
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 14:19:06 +0200

    Added a middleware that echoes the request body to the response

---
+++ Middlewares/Middlewares/EchoBodyMiddleWare.cs
@@ -0,0 +1,41 @@
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Middlewares
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class EchoBodyMiddleWare
+    {
+        private readonly AppFunc _next;
+
+        public EchoBodyMiddleWare(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            var context = new OwinContext(environment);
+
+            if (context.Request.Method == "POST")
+            {
+                var response = context.Response;
+
+                using (var reader = new StreamReader(context.Request.Body))
+                {
+                    var body = await reader.ReadToEndAsync();
+                    var bytes = Encoding.UTF8.GetBytes(body);
+
+                    await response.WriteAsync(bytes);
+                }
+            }
+
+            await _next(environment);
+        }
+    }
+}
