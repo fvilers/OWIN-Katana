1d6e006448d0775ee11f0b33b0761adefd566892
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 13:24:49 +0200

    Moved the stopwatch middleware to the app scope

--- Conditionals/Conditionals/Startup.cs
+++ Conditionals/Conditionals/Startup.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
 using Owin;
+using System;
+using System.Diagnostics;
 
 namespace Conditionals
 {
@@ -8,23 +10,22 @@ namespace Conditionals
     {
         public void Configuration(IAppBuilder app)
         {
-            app.Map("/mic", nestedApp =>
-            {
-                nestedApp.Use(async (context, next) =>
-                {
-                    var sw = new Stopwatch();
-                    sw.Start();
+            app.Map("/mic", nestedApp => nestedApp.Run(context => context.Response.WriteAsync("Hello, Microsoft Innovation Center!\r\n")));
 
-                    await next.Invoke();
-                    sw.Stop();
-
-                    await context.Response.WriteAsync(String.Format("The pipeline execution took {0} ms\r\n", sw.ElapsedMilliseconds));
-                });
-
-                nestedApp.Run(context => context.Response.WriteAsync("Hello, Microsoft Innovation Center!\r\n"));
-            });
+            app.Use(StopwatchMiddleware);
 
             app.Run(context => context.Response.WriteAsync("Hello, World!\r\n"));
+        }
+
+        private static async Task StopwatchMiddleware(IOwinContext context, Func<Task> next)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            await next.Invoke();
+            sw.Stop();
+
+            await context.Response.WriteAsync(String.Format("The pipeline execution took {0} ms\r\n", sw.ElapsedMilliseconds));
         }
     }
 }
