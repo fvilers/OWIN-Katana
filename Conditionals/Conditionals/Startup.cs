d497727d36dd9d2171192877c8a4194ec78d8bcf
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 13:14:17 +0200

    Added a nested app with conditional on path

---
+++ Conditionals/Conditionals/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using Owin;
+
+namespace Conditionals
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/mic", nestedApp =>
+            {
+                nestedApp.Use(async (context, next) =>
+                {
+                    var sw = new Stopwatch();
+                    sw.Start();
+
+                    await next.Invoke();
+                    sw.Stop();
+
+                    await context.Response.WriteAsync(String.Format("The pipeline execution took {0} ms\r\n", sw.ElapsedMilliseconds));
+                });
+
+                nestedApp.Run(context => context.Response.WriteAsync("Hello, Microsoft Innovation Center!\r\n"));
+            });
+
+            app.Run(context => context.Response.WriteAsync("Hello, World!\r\n"));
+        }
+    }
+}
