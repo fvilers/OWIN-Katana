05281fa730cd2dc55976a1ee8cbd507ebdbedf8e
Fabian Vilers <fabian.vilers@dev-one.com>
2014-09-07 13:27:08 +0200

    Removed stopwatch middleware for sake of brevety

--- Conditionals/Conditionals/Startup.cs
+++ Conditionals/Conditionals/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System.Threading.Tasks;
-using Microsoft.Owin;
-using Owin;
-using System;
-using System.Diagnostics;
+﻿using Owin;
 
 namespace Conditionals
 {
@@ -10,22 +6,14 @@ namespace Conditionals
     {
         public void Configuration(IAppBuilder app)
         {
-            app.Map("/mic", nestedApp => nestedApp.Run(context => context.Response.WriteAsync("Hello, Microsoft Innovation Center!\r\n")));
+            app.Map("/mic", nestedApp =>
+            {
+                nestedApp.Map("/renaud", renaudApp => renaudApp.Run(context => context.Response.WriteAsync("Hello, Renaud!")));
 
-            app.Use(StopwatchMiddleware);
+                nestedApp.Run(context => context.Response.WriteAsync("Hello, Microsoft Innovation Center!\r\n"));
+            });
 
             app.Run(context => context.Response.WriteAsync("Hello, World!\r\n"));
-        }
-
-        private static async Task StopwatchMiddleware(IOwinContext context, Func<Task> next)
-        {
-            var sw = new Stopwatch();
-            sw.Start();
-
-            await next.Invoke();
-            sw.Stop();
-
-            await context.Response.WriteAsync(String.Format("The pipeline execution took {0} ms\r\n", sw.ElapsedMilliseconds));
         }
     }
 }
